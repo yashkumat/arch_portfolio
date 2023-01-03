ea464c12707291f39e2b1ddccc45de790d8e704b
yash kumat <ykumat@gmail.com>
2023-01-04 04:06:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ function changeBg (url) {
 function handlePageLoaded(){
     var heights = window.innerHeight;
     document.getElementById("content").style.height = heights + "px";
+    document.getElementById("bg").style.height = heights + "px";
     setTimeout(()=>{
         document.getElementById("loading-screen").style.display = "none";
     }, 1000)
